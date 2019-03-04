b4bce291bc8e353a269ccd97ae5210afa0f17534
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-03-04 22:15:53 +0300

    updated tar z collector search

--- scripts/tar-z-collector/pages_with_archives/clear.sh
+++ scripts/tar-z-collector/pages_with_archives/clear.sh
@@ -4,4 +4,3 @@ set -e
 cd "$(dirname $0)"
 
 truncate --size=0 "url.list"
-truncate --size=0 "invalid_url.list"
--- scripts/tar-z-collector/pages_with_archives/update.rb
+++ scripts/tar-z-collector/pages_with_archives/update.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require "json"
+require "digest"
 
 require "../query"
 
@@ -19,7 +20,8 @@ def get_search_text
     "\"#{TARGET_ARCHIVE_EXTENSION}\"",
     %w[index directory listing ftp].shuffle.join("|")
   ]
-  .shuffle.join " "
+  .shuffle
+  .join(" ")
 end
 
 # -- stats --
@@ -44,25 +46,36 @@ SEARCH_ENDPOINT_REGEXP = Regexp.new(
 )
 .freeze
 
-def get_search_endpoints_from_stats(stats_endpoint)
-  uri = URI stats_endpoint
-  data = get_http_content uri
+def get_search_endpoints_from_stats_endpoint(stats_endpoint)
+  STDERR.puts "- processing stats endpoint: #{stats_endpoint}"
 
-  search_endpoints = data.scan(SEARCH_ENDPOINT_REGEXP).flatten
-  STDERR.puts "received #{search_endpoints.length} search endpoints from stats endpoint: #{stats_endpoint}"
+  begin
+    uri  = URI stats_endpoint
+    data = get_http_content uri
+
+    search_endpoints = data.scan(SEARCH_ENDPOINT_REGEXP).flatten
+
+  rescue StandardError => error
+    STDERR.puts error
+    return []
+  end
+
+  STDERR.puts "received #{search_endpoints.length} search endpoints"
 
   search_endpoints
 end
 
 def get_search_endpoints
+  STDERR.puts "-- searching among stats endpoints"
+
   search_endpoints = STATS_ENDPOINTS
     .shuffle
-    .map { |stats_endpoint| get_search_endpoints_from_stats stats_endpoint }
+    .map { |stats_endpoint| get_search_endpoints_from_stats_endpoint stats_endpoint }
     .flatten
     .sort
     .uniq
 
-  STDERR.puts "received #{search_endpoints.length} search endpoints"
+  STDERR.puts "-- received #{search_endpoints.length} search endpoints"
 
   search_endpoints
 end
@@ -70,13 +83,13 @@ end
 # -- search --
 
 def get_urls_from_search_endpoint(search_endpoint)
-  STDERR.puts "-----"
-  STDERR.puts "processing search endpoint: #{search_endpoint}"
+  STDERR.puts "- listing search endpoint: #{search_endpoint}"
 
   begin
     uri = URI "#{search_endpoint}/search"
   rescue StandardError
-    STDERR.puts "invalid url"
+    STDERR.puts "invalid api url"
+    return []
   end
 
   urls   = []
@@ -85,7 +98,7 @@ def get_urls_from_search_endpoint(search_endpoint)
 
   loop do
     params[:pageno] = page
-    uri.query = URI.encode_www_form params
+    uri.query       = URI.encode_www_form params
 
     begin
       data = get_http_content uri
@@ -135,8 +148,7 @@ def get_urls
     .sort
     .uniq
 
-  STDERR.puts "-----"
-  STDERR.puts "received #{urls.length} urls from all search endpoints"
+  STDERR.puts "-- received #{urls.length} urls from all search endpoints"
 
   urls
 end
@@ -167,94 +179,96 @@ LISTING_WITH_ARCHIVES_REGEXP = Regexp.new(
 )
 .freeze
 
-def page_with_archives?(url)
+def page_with_archives?(url, urls_hash, invalid_urls_hash)
+  STDERR.puts "- checking page, url: #{url}"
+
   begin
     uri = URI url
-  rescue StandardError
-    STDERR.puts "invalid url"
-    return false, true
-  end
 
-  case uri.scheme
-  when "ftp"
-    begin
+    case uri.scheme
+    when "ftp"
       data, is_listing = get_file_or_listing_from_ftp uri
-    rescue StandardError => error
-      STDERR.puts error
-      return false, true
+      regexp           = is_listing ? LISTING_WITH_ARCHIVES_REGEXP : PAGE_WITH_ARCHIVES_REGEXP
+
+    when "http", "https"
+      data   = get_http_content uri
+      regexp = PAGE_WITH_ARCHIVES_REGEXP
+
+    else
+      raise "uknown uri scheme: #{scheme}"
     end
 
-    regexp = is_listing ? LISTING_WITH_ARCHIVES_REGEXP : PAGE_WITH_ARCHIVES_REGEXP
-    [data =~ regexp, false]
-
-  when "http", "https"
-    begin
-      data = get_http_content uri
-    rescue StandardError => error
-      STDERR.puts error
-      return false, true
-    end
-
-    [data =~ PAGE_WITH_ARCHIVES_REGEXP, false]
-
-  else
-    STDERR.puts "uknown page uri scheme: #{scheme}"
-    return false, true
+  rescue StandardError => error
+    STDERR.puts error
+    return false
   end
-end
 
-def check_page_with_archives(url)
-  STDERR.puts "-----"
-  STDERR.puts "processing page with archives, url: #{url}"
+  result = data =~ regexp
+  digest = Digest::SHA256.hexdigest data
 
-  result, is_error = page_with_archives? url
   STDERR.puts "page is #{result ? 'valid' : 'invalid'}"
 
-  [result, is_error]
+  if result
+    adding_to_hash     = urls_hash
+    removing_from_hash = invalid_urls_hash
+  else
+    adding_to_hash     = invalid_urls_hash
+    removing_from_hash = urls_hash
+  end
+
+  adding_to_hash[url] = digest
+  removing_from_hash.delete url
+
+  result
 end
 
-def get_filtered_urls(urls)
-  filtered_invalid_urls = []
+def filter_urls(urls_hash, invalid_urls_hash)
+  filtered_urls_length         = 0
+  filtered_invalid_urls_length = 0
 
-  filtered_urls = urls
+  get_urls
     .shuffle
-    .select do |url|
-      result, is_error = check_page_with_archives url
-      filtered_invalid_urls << url unless is_error
-      result
+    .reject { |url| urls_hash.key?(url) || invalid_urls_hash.key?(url) }
+    .each do |url|
+      if page_with_archives? url, urls_hash, invalid_urls_hash
+        filtered_urls_length += 1
+      else
+        filtered_invalid_urls_length += 1
+      end
     end
 
-  STDERR.puts "-----"
-  STDERR.puts "received #{filtered_urls.length} filtered urls"
+  STDERR.puts "-- filtered #{filtered_urls_length} urls, #{filtered_invalid_urls_length} invalid urls"
 
-  [filtered_urls, filtered_invalid_urls]
+  nil
 end
 
 # -- files --
 
-def read_urls(path)
-  File
-    .read(path)
+def read_urls_hash(path)
+  data = File.read(path)
+  data
     .split("\n")
     .map(&:strip)
     .reject(&:empty?)
+    .map { |row| row.split(" ").map(&:strip).reject(&:empty?) }
+    .to_h
 end
 
-def write_urls(path, urls)
-  File.write path, urls.join("\n")
+def write_urls_hash(path, urls_hash)
+  data = urls_hash
+    .map { |values| values.join(" ") }
+    .join("\n")
+  File.write path, data
+  nil
 end
 
-urls_path         = ARGV[0]
-invalid_urls_path = ARGV[1]
+urls_hash_path         = ARGV[0]
+invalid_urls_hash_path = ARGV[1]
 
-urls         = read_urls urls_path
-invalid_urls = read_urls invalid_urls_path
+urls_hash         = read_urls_hash urls_hash_path
+invalid_urls_hash = read_urls_hash invalid_urls_hash_path
 
-new_urls                             = get_urls - urls - invalid_urls
-filtered_urls, filtered_invalid_urls = get_filtered_urls new_urls
+filter_urls urls_hash, invalid_urls_hash
 
-urls         = (urls + filtered_urls).sort.uniq
-invalid_urls = (invalid_urls + filtered_invalid_urls).sort.uniq
-
-write_urls urls_path, urls
-write_urls invalid_urls_path, invalid_urls
+write_urls_hash urls_hash_path, urls_hash
+write_urls_hash invalid_urls_hash_path, invalid_urls_hash
