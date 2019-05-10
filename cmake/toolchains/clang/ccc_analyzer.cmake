950f2ca100a07de5eb28b91c0408782bf9d17f74
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-05-10 17:27:57 +0300

    implementing compatibility with osx

--- cmake/toolchains/clang/ccc_analyzer.cmake
+++
@@ -1,9 +0,0 @@
-set (CMAKE_SYSTEM_NAME "Linux")
-
-set (CMAKE_C_COMPILER "ccc-analyzer")
-set (CMAKE_C_COMPILER_AR "llvm-ar")
-set (CMAKE_C_COMPILER_RANLIB "llvm-ranlib")
-
-set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror" CACHE STRING "cmake C flags")
-
-set (CMAKE_HAVE_IPO false CACHE BOOL "status of IPO support")
