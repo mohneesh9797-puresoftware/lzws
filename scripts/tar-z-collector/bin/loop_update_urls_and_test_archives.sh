e29e4837f67d1cd49fbd40569722bd9f673c658b
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-05-12 21:24:35 +0300

    scripts cosmetics

--- scripts/tar-z-collector/bin/loop_update_urls_and_test_archives.sh
+++ scripts/tar-z-collector/bin/loop_update_urls_and_test_archives.sh
@@ -3,7 +3,12 @@ set -e
 
 cd "$(dirname $0)"
 
+UPDATE_URLS_TIMES=10
+
 while true; do
-  ./update_urls.sh
+  for i in {1..$UPDATE_URLS_TIMES}; do
+    ./update_urls.sh
+  done
+
   ./test_archives.sh
 done
