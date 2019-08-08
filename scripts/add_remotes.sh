171a7b1386de3a592ee69e1d9fc4b33b08df91ce
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-08-08 22:45:08 +0300

    fixed remote urls

--- scripts/add_remotes.sh
+++ scripts/add_remotes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-git remote add github     "ssh://git@github.com:andrew-aladev/lzws.git"    || true
-git remote add bitbucket  "ssh://git@bitbucket.org:andrew-aladev/lzws.git" || true
+git remote add github     "git@github.com:andrew-aladev/lzws.git"    || true
+git remote add bitbucket  "git@bitbucket.org:andrew-aladev/lzws.git" || true
