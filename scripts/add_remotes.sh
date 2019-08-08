0f51fab7b5dfa5094e868490019d470a3f040c25
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-08-08 23:57:20 +0300

    added gitlab remote

--- scripts/add_remotes.sh
+++ scripts/add_remotes.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 
-git remote add github     "git@github.com:andrew-aladev/lzws.git"    || true
-git remote add bitbucket  "git@bitbucket.org:andrew-aladev/lzws.git" || true
+git remote add github    "git@github.com:andrew-aladev/lzws.git"    || true
+git remote add bitbucket "git@bitbucket.org:andrew-aladev/lzws.git" || true
+git remote add gitlab    "git@gitlab.com:andrew-aladev/lzws.git"    || true
