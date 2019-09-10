9161b91a828c7d18b7f76b7333588998489c1711
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-09-11 01:41:12 +0300

    scripts cosmetics

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
 
+cd "$(dirname $0)"
+
 git fetch --all || true
 git remote | xargs -n1 -I {} git rebase "{}/master" || true
+
+cd "tar-z-collector"
+gem install bundler
+bundle update
