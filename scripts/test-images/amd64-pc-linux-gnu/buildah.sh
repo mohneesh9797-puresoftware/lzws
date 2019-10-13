c49bc722e85548f37505812f32380950b5a1e2d9
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-10-13 21:24:34 +0300

    test image entrypoint cosmetics

--- scripts/test-images/amd64-pc-linux-gnu/buildah.sh
+++ scripts/test-images/amd64-pc-linux-gnu/buildah.sh
@@ -9,10 +9,10 @@ source "../utils.sh"
 DOCKER_IMAGE="${DOCKER_IMAGE_PREFIX}_amd64-pc-linux-gnu"
 
 CONTAINER=$(buildah from "docker.io/$DOCKER_USERNAME/test_amd64-pc-linux-gnu:latest")
-buildah config --label maintainer="$MAINTAINER" --entrypoint "/home/data/entrypoint.sh" "$CONTAINER"
+buildah config --label maintainer="$MAINTAINER" --entrypoint "/home/entrypoint.sh" "$CONTAINER"
 
-run mkdir -p /home/data
-copy ../entrypoint.sh /home/data/
+run mkdir -p /home
+copy ../entrypoint.sh /home/
 
 copy root/ /
 build emerge -v dev-vcs/git dev-util/cmake dev-libs/gmp
