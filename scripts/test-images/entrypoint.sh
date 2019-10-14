f9be590468b88f4660cef8188583b901690eecf2
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-10-14 13:58:50 +0300

    cosmetics

--- scripts/test-images/entrypoint.sh
+++ scripts/test-images/entrypoint.sh
@@ -6,7 +6,7 @@ cd "$(dirname $0)"
 env-update
 source /etc/profile
 
-git clone "https://github.com/andrew-aladev/lzws.git" --single-branch --branch master --depth 1 "lzws"
+git clone "https://github.com/andrew-aladev/lzws.git" --single-branch --branch "master" --depth 1 "lzws"
 cd "lzws"
 
 ./scripts/toolchains.sh
