69d9b66164c9c9cb175dfb2fdf2c0b6ebf1dff44
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-02-27 20:17:55 +0100

    updated tar z collector

--- scripts/tar-z-collector/update_urls/stats.rb
+++ scripts/tar-z-collector/update_urls/stats.rb
@@ -1,80 +1,36 @@
+require "json"
 require "uri"
 
 require_relative "../common/format"
 require_relative "../common/query"
 
-# https://github.com/asciimoo/searx/wiki/Searx-instances
-STATS_URLS = %w[
-  https://stats.searx.xyz
-  https://stats.searx.xyz/tor.html
-]
-.freeze
+STATS_URL = "https://searx.space/data/instances.json".freeze
 
-# <a href='https://searx.at'>https://searx.at</a>
-# <a href="https://searx.at">https://searx.at</a>
-# <a href=https://searx.at>https://searx.at</a>
-SEARCH_URL_REGEXP = Regexp.new(
-  "
-    <a
-      [[:space:]]*
-      href[[:space:]]*=[[:space:]]*
-      (?:
-          '
-          ([^']+)
-          '
-        |
-          \"
-          ([^\"]+)
-          \"
-        |
-          ([^[:space:]>]+)
-      )
-      [[:space:]]*
-    >
-      [[:space:]]*
-      (?:
-          \\1
-        |
-          \\2
-        |
-          \\3
-      )
-      [[:space:]]*
-    </a>
-  ",
-  Regexp::IGNORECASE | Regexp::MULTILINE | Regexp::EXTENDED
-)
-.freeze
-
-def get_search_urls_from_stats_url(url)
-  warn "- processing stats url: #{url}"
+def get_search_urls
+  warn "- processing stats url: #{STATS_URL}"
 
   begin
-    uri  = URI url
+    uri  = URI STATS_URL
     data = get_http_content uri
+    data = JSON.parse data
 
-    search_urls = data.scan(SEARCH_URL_REGEXP).flatten.compact
+    instances   = data["instances"]
+    search_urls = [
+      instances.keys.map(&:to_s),
+      instances.values.flat_map { |instance| instance["alternativeUrls"].keys.map(&:to_s) }
+    ]
+    .flatten
+    .shuffle
+    .sort
+    .uniq
 
   rescue StandardError => error
     warn error
-    return []
+    search_urls = []
   end
 
   text = colorize_length search_urls.length
   warn "received #{text} search urls"
-
-  search_urls
-end
-
-def get_search_urls
-  search_urls = STATS_URLS
-    .shuffle
-    .flat_map { |stats_url| get_search_urls_from_stats_url stats_url }
-    .sort
-    .uniq
-
-  text = colorize_length search_urls.length
-  warn "-- received #{text} search urls from all stats urls"
 
   search_urls
 end
