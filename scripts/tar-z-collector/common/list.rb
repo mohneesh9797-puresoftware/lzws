d9e310da9fd99d48ef464645e403871f824c11db
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-04-04 16:57:25 +0300

    used max possible xz compression

--- scripts/tar-z-collector/common/list.rb
+++ scripts/tar-z-collector/common/list.rb
@@ -1,15 +1,21 @@
 require "xz"
 
+COMPRESS_OPTIONS = {
+  :level   => 9,
+  :check   => :sha256,
+  :extreme => true
+}
+.freeze
+
 def read_list(path)
-  XZ
-    .decompress(File.read(path))
+  XZ.decompress(File.read(path))
     .split("\n")
     .map(&:strip)
     .reject(&:empty?)
 end
 
 def write_list(path, list)
-  data = XZ.compress list.join("\n")
+  data = XZ.compress list.join("\n"), **COMPRESS_OPTIONS
   File.write path, data
 
   nil
