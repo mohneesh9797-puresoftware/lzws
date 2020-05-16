dc167edc402fe37d59e05816dca7aadb5d96e4fa
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-05-16 02:11:12 +0200

    cosmetics

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,5 +8,4 @@ git fetch --all || true
 git fetch --tags || true
 git remote | xargs -n1 -I {} git rebase "{}/$(git branch --show-current)" || true
 
-git submodule sync
 git submodule update --init --recursive --remote
